c8694df399590064a2230c255ab8b66506b18127
Andrew Arnott <andrewarnott@live.com>
2024-12-23 18:14:04 -0700

    Revert "Merge latest Library.Template" (#79)

--- test/Nerdbank.QRCodes.Tests/QRDecoderTests.cs
+++ test/Nerdbank.QRCodes.Tests/QRDecoderTests.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Xunit.Abstractions;
+
 public class QRDecoderTests(ITestOutputHelper logger) : TestBase
 {
 	public static readonly string[] SupportedFileTypes = ["bmp", "gif", "png", "jpg"];
